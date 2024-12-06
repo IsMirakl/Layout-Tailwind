1909ef393b56805882ecd612fe3c1c60b287643e
Daniil <den2007fad@gmail.com>
2024-12-06 20:45:55 +0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
         "castom_color": "#DAD8D8",
         "castom_color2": "#B00000",
         "castom_color3": "#D6D4D4E0",
+        "castom_color4": "#808080",
+        "castom_color_btn": "#B000004D",
       },
+    },
+    boxShadow: {
+      'custom': '0px 0px 64px 0px rgba(0, 0, 0, 0.5)',
     },
   },
   plugins: [],
